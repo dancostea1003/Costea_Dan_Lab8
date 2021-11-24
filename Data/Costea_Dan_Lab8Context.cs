803b209089aa086c993d7767402090e4c9b004a2
DAN-CĂTĂLIN COSTEA <dan.costea@stud.ubbcluj.ro>
2021-11-24 13:27:14 +0200

    Add project files.

---
+++ Data/Costea_Dan_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Costea_Dan_Lab8.Models;
+
+namespace Costea_Dan_Lab8.Data
+{
+    public class Costea_Dan_Lab8Context : DbContext
+    {
+        public Costea_Dan_Lab8Context (DbContextOptions<Costea_Dan_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Costea_Dan_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Costea_Dan_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Costea_Dan_Lab8.Models.Category> Category { get; set; }
+    }
+}
